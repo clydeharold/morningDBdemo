1424cfb4589e921cfdf0ecf24c65dcabaf110a5f
clydeharold <ch.paronda@gmail.com>
2021-11-06 09:31:24 +0800

    added codes for dbhandler oncreate and onupgrade

--- app/src/main/java/com/example/morningdbdemo/data/DatabaseHandler.java
+++ app/src/main/java/com/example/morningdbdemo/data/DatabaseHandler.java
@@ -1,4 +1,45 @@
 package com.example.morningdbdemo.data;
 
-public class DatabaseHandler {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import com.example.morningdbdemo.R;
+import com.example.morningdbdemo.util.Util;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+
+    public DatabaseHandler(Context context) {
+        super(context, Util.DATABASE_NAME, null, Util.DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String CREATE_PRODUCT_TABLE = "CREATE TABLE " + Util.TABLE_NAME + "("
+                + Util.KEY_ID + " INTEGER PRIMARY KEY,"
+                + Util.KEY_NAME + " TEXT,"
+                + Util.KEY_PRICE + " DOUBLE,"
+                + Util.KEY_QUANTITY + " INTEGER" + ")";
+
+        db.execSQL(CREATE_PRODUCT_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String DROP_TABLE = String.valueOf(R.string.drop_table);
+        db.execSQL(DROP_TABLE,new String[]{Util.DATABASE_NAME});
+    }
+
+    //CRUD
+
+    //Create
+
+    //Retrieve
+
+    //Update
+    
+    //Delete
 }
